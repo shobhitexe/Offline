92e5bc164d637da0855359613d05c3af4f8fda4f
Shobhit <sguptamds94@gmail.com>
2024-12-17 10:16:49 +0530

    Timer Update

--- apps/web/src/components/Sports/Match/Timer.tsx
+++ apps/web/src/components/Sports/Match/Timer.tsx
@@ -1,6 +1,44 @@
 "use client";
 
-import Countdown from "react-countdown";
+import Countdown, { CountdownRenderProps } from "react-countdown";
+import { Tv } from "lucide-react";
+
+const renderer = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+  completed,
+}: CountdownRenderProps) => {
+  if (completed) {
+    return (
+      <div className="flex items-center gap-2 justify-center font-semibold">
+        LIVE <Tv className="w-4 h-4 -top-px relative" />
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-4 gap-4 text-center">
+        <div>
+          <div className="text-xl">{days}</div>
+          <div className="text-xs">Days</div>
+        </div>
+        <div>
+          <div className="text-xl">{hours}</div>
+          <div className="text-xs">Hours</div>
+        </div>
+        <div>
+          <div className="text-xl">{minutes}</div>
+          <div className="text-xs">Min</div>
+        </div>
+        <div>
+          <div className="text-xl">{seconds}</div>
+          <div className="text-xs">Sec</div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default function Timer({
   eventName,
@@ -14,29 +52,7 @@ export default function Timer({
       <div className="absolute left-1/2 -translate-x-1/2 -top-4 bg-[#1a1f4d] text-white rounded-lg p-4 min-w-[300px]">
         <h2 className="text-center text-sm mb-2">{eventName}</h2>
 
-        <Countdown
-          date={new Date(eventTime)}
-          renderer={({ days, hours, minutes, seconds }) => (
-            <div className="grid grid-cols-4 gap-4 text-center">
-              <div>
-                <div className="text-xl">{days}</div>
-                <div className="text-xs">Days</div>
-              </div>
-              <div>
-                <div className="text-xl">{hours}</div>
-                <div className="text-xs">Hours</div>
-              </div>
-              <div>
-                <div className="text-xl">{minutes}</div>
-                <div className="text-xs">Min</div>
-              </div>
-              <div>
-                <div className="text-xl">{seconds}</div>
-                <div className="text-xs">Sec</div>
-              </div>
-            </div>
-          )}
-        />
+        <Countdown date={new Date(eventTime)} renderer={renderer} />
       </div>
     </div>
   );
